ef8779c339278dfddd6f4d960755a638c79c94dc
Antoine MINEAU <amineau@e2r6p13.42.fr>
2016-01-07 18:19:27 +0100

    l

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: amineau <amineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 15:58:35 by amineau           #+#    #+#             */
-/*   Updated: 2016/01/07 17:35:21 by amineau          ###   ########.fr       */
+/*   Updated: 2016/01/07 18:09:11 by amineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 # include "libft/libft.h"
 # include "minilibx/mlx.h"
 # include <math.h>
+
+typedef struct	s_coor
+{
+	int	x0;
+	int	y0;
+	int	x1;
+	int	y1;
+	struct s_coord;
+}				t_coor;
 
 #endif
--- main.c
+++ main.c
@@ -6,11 +6,38 @@
 /*   By: amineau <amineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 16:02:28 by amineau           #+#    #+#             */
-/*   Updated: 2016/01/07 17:34:57 by amineau          ###   ########.fr       */
+/*   Updated: 2016/01/07 18:19:17 by amineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void TraceSegmentPointMilieu (s_coor cr, unsigned char couleur)
+{
+	int dx = cr.x0 - cr.x0;
+	int dy = cr.y1 - cr.y0;
+	int dp = 2 * dy - dx;
+	int deltaE = 2 * dy;
+	int deltaNE = 2 * (dy - dx);
+	int x = cr.x0;
+	int y = cr.y0;
+		mlx_pixel_put(mlx, win, x, y, 0x00ffff);
+	while (x < x1)
+	{
+		if (dp <= 0)
+		{
+			dp += deltaE;
+			x++;
+		}
+		else
+		{
+			dp += deltaNE;
+			x++;
+			y++;
+		}
+		mlx_pixel_put(mlx, win, x, y, 0x00ffff);
+	}
+}
 
 int main()
 {
@@ -29,6 +56,6 @@ int main()
 		while (++y < 800)
 			mlx_pixel_put(mlx, win, i, y, 0x00ffff);
 	}
-		mlx_loop(mlx);
+	mlx_loop(mlx);
 	return (0);
 }
